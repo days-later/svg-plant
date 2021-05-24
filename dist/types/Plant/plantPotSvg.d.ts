8811c01756b18c6b947ab47efd4c51a5d7502038
Ff <fridtjof@gmail.com>
2021-05-24 14:05:02 +0200

    switch to vite, overhaul demo page

--- dist/types/Plant/plantPotSvg.d.ts
+++
@@ -1,3 +0,0 @@
-import { attributeSet } from '../types';
-declare function plantPotSvg(pathAttr: attributeSet): SVGElement;
-export { plantPotSvg };
--- dist/types/util/DevTools.d.ts
+++
@@ -1,9 +0,0 @@
-import { PlantBody } from "../Plant/PlantBody";
-import { GenusConstructor } from '../types';
-declare const testPlantBodySize: (genus: GenusConstructor, n?: number) => (q?: number) => {
-    width: number;
-    height: number;
-};
-declare const findSeed: (genus: GenusConstructor, test: (p: PlantBody) => boolean, timeoutMs?: number) => string | undefined;
-declare const testPerformance: (genus: GenusConstructor, durationMs?: number) => void;
-export { testPlantBodySize, findSeed, testPerformance, };
